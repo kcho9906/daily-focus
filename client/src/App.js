c8ef155b305263fe55c410c4a7498cd8cfcd398d
sannan06 <sannanhafeez06@gmail.com>
2021-03-10 17:41:01 +1300

    #5 client setup (#17)
    * Add prettier as dev dependency.
    
    Also added a pre-commit hook to automatically format any staged files, and formatted all existing files in client folder.
    
    * Add node-sass and material-ui/core as dependencies
    
    * Add ESLint as dependency, and create configuration file for it
    
    * Add 'Ropa Sans' as font to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,28 @@
-import logo from './logo.svg';
-import './style.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./style.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/components/index.js</code> and save to reload.
-          <br/>
-          ** DAILY FOCUS **
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/components/index.js</code> and save to reload.
+                    <br />
+                    ** DAILY FOCUS **
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from '.';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { test, expect } from "@testing-library/jest-dom";
+import App from ".";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
 });
