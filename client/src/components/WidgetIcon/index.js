6e42735d024c42d775711edbd7129daf30afde46
Steven <kuanchenho@gmail.com>
2021-03-18 10:36:19 +1300

    Create component for each widget in select widget modal

--- client/src/components/WidgetIcon/index.js
+++ client/src/components/WidgetIcon/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Grid from "@material-ui/core/Grid";
@@ -6,20 +6,28 @@ import Grid from "@material-ui/core/Grid";
 import styles from "./style.module.scss";
 
 function WidgetIcon({ image, label, selectedWidgets, setSelectedWidgets }) {
+    const [selected, setSelected] = useState(false);
+
     const handleClick = () => {
-        if (!selectedWidgets.includes(label)) {
-            //checking weather array contain the id
-            setSelectedWidgets([...selectedWidgets, label]);
-        } else {
-            setSelectedWidgets(
-                selectedWidgets.filter((selectedWidget) => selectedWidget !== label)
-            ); //deleting
-        }
+        setSelected(!selected);
+
+        // if (!selectedWidgets.includes(label)) {
+        //     //checking weather array contain the id
+        //     setSelectedWidgets([...selectedWidgets, label]);
+        // } else {
+        //     setSelectedWidgets(
+        //         selectedWidgets.filter((selectedWidget) => selectedWidget !== label)
+        //     ); //deleting
+        // }
     };
 
     return (
         <Grid item xs={6}>
-            <Card className={styles.card} onClick={handleClick} raised>
+            <Card
+                className={`${styles.card} ${selected && styles.selectedCard}`}
+                onClick={handleClick}
+                raised
+            >
                 <img src={image} className={styles.media} alt={label} />
                 <CardContent className={styles.cardContent}>{label}</CardContent>
             </Card>
