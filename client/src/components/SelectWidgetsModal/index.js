6daa6c5969a0fe8436f65236cb80fd0aeea21050
Steven <kuanchenho@gmail.com>
2021-03-18 19:21:08 +1300

    Updated styling

--- client/src/components/SelectWidgetsModal/index.js
+++ client/src/components/SelectWidgetsModal/index.js
@@ -1,19 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import Dialog from "@material-ui/core/Dialog";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Grid from "@material-ui/core/Grid";
-
+import { makeStyles } from "@material-ui/core";
 import toDoList from "../../images/toDoList.svg";
 import calendar from "../../images/calendar.svg";
 import timer from "../../images/timer.svg";
 import styles from "./style.module.scss";
 import WidgetIcon from "../WidgetIcon";
 
+const useStyles = makeStyles({
+    button: {
+        background: "#30A0F5",
+        borderRadius: "20px",
+        textTransform: "none",
+        fontFamily: "'Ropa Sans', sans-serif",
+    },
+});
+
 function SelectWidgetsModal() {
     const [open, setOpen] = useState(false);
     const [selectedWidgets, setSelectedWidgets] = useState([]);
+
+    const classes = useStyles();
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -23,16 +34,17 @@ function SelectWidgetsModal() {
         setOpen(false);
     };
 
-    useEffect(() => {
-        console.log(selectedWidgets);
-    }, [selectedWidgets]);
-
     return (
         <div>
-            <Button variant="contained" color="primary" onClick={handleClickOpen}>
+            <Button
+                className={classes.button}
+                variant="contained"
+                color="primary"
+                onClick={handleClickOpen}
+            >
                 Manage Widgets
             </Button>
-            <Dialog className={styles.dialog} open={open} onClose={handleClose}>
+            <Dialog className={classes.dialog} open={open} onClose={handleClose}>
                 <DialogTitle>
                     <p className={styles.title}>Select your widgets</p>
                 </DialogTitle>
