173bd7024efd153cd4992e0ab7daf1ab4a76aecb
Steven <kuanchenho@gmail.com>
2021-03-19 10:19:38 +1300

    migrate test configuration to higher level

--- client/src/components/SelectWidgetsModal/SelectWidgetsModal.test.js
+++ client/src/components/SelectWidgetsModal/SelectWidgetsModal.test.js
@@ -1,11 +1,8 @@
 import React from "react";
 import SelectWidgetsModal from ".";
-import Enzyme, { ReactWrapper, shallow } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
 import Dialog from "@material-ui/core/Dialog";
 import Button from "@material-ui/core/Button";
-
-Enzyme.configure({ adapter: new Adapter() });
 
 test("Button opens modal when clicked", () => {
     const wrapper = shallow(<SelectWidgetsModal />);
